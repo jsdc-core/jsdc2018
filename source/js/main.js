28928e985c03934e913f64337d3e40de411caa82
WeBber <bird23074035@gmail.com>
2018-08-30 01:06:02 +0800

    add time clock

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,13 @@
 (function($) {
     if (location.pathname === '/')
+      function toTwo(num) {
+        return num < 10 ? '0' + num : '' + num;
+      }
       var countdown = setInterval(function(){
         var specifyDay = null;
-        var nightTicketEndDay = new Date('2018/11/16 22:00:00').getTime();
-        var confStartDay = new Date('2018/11/17 09:00:00').getTime();
-        var confEndtDay = new Date('2018/11/18 17:00:00').getTime();
+        var nightTicketEndDay = new Date('2018/08/16 22:00:00').getTime();
+        var confStartDay = new Date('2018/08/28 09:00:00').getTime();
+        var confEndtDay = new Date('2018/09/29 17:00:00').getTime();
         // var countdownTitle = document.getElementsByClassName('content')[0];
         var ticketButtonText = document.getElementsByClassName('ticket-booking')[0];
 
@@ -36,18 +39,26 @@
           seconds: 1000,
           minutes: 1000*60,
           hours: 1000*60*60,
-          date: 1000*60*60*24,
+          day: 1000*60*60*24,
         }
 
-        var date = Math.floor(diff / util.date);
-        var hours = Math.floor((diff - (date * util.date)) / util.hours);
-        var minutes = Math.floor((diff - ((date * util.date) + (hours * util.hours))) / util.minutes);
-        var seconds = Math.floor((diff - ((date * util.date) + (hours * util.hours) + (minutes * util.minutes))) / util.seconds);
+        var day = Math.floor(diff / util.day);
+        var hours = Math.floor((diff / util.hours) % 24);
+        var minutes = Math.floor((diff / util.minutes) % 60);
+        var seconds = Math.floor((diff / util.seconds) % 60);
 
-        $('.date').text(date.toString().length === 1?'0'+ date:date);
-        $('.hours').text(hours.toString().length === 1?'0'+ hours:hours);
-        $('.minutes').text(minutes.toString().length === 1?'0'+ minutes:minutes);
-        $('.seconds').text(seconds.toString().length === 1?'0'+ seconds:seconds);
+        var str = toTwo(day) + toTwo(hours) + toTwo(minutes) + toTwo(seconds);
+        $('.time-content .num').each(function(i, ele){
+          $(ele).html('<img width=75px src="images/number/'+str.charAt(i)+'.png">');
+          console.log(str.charAt(i))
+        });
+        console.log('L46');
+        // console.log(`${str}`);
+
+        // $('.date').text(day.toString().length === 1?'0'+ day:day);
+        // $('.hours').text(hours.toString().length === 1?'0'+ hours:hours);
+        // $('.minutes').text(minutes.toString().length === 1?'0'+ minutes:minutes);
+        // $('.seconds').text(seconds.toString().length === 1?'0'+ seconds:seconds);
       }, 1000);
 
 })(jQuery);
